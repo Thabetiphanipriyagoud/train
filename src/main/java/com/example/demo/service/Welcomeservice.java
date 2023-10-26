63f9f713507c223b7743de0d9502fcb104cc2183
phani <user603@Docker-I-33.alchemycloud.co.in>
2023-10-26 11:41:55 +0530

    commit welcome service

--- src/main/java/com/example/demo/service/Welcomeservice.java
+++ src/main/java/com/example/demo/service/Welcomeservice.java
@@ -1,14 +1,16 @@
-package com.example.demo.controller;
+package com.example.demo.service;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Welcome {
 
-	public String[] getcity()
-	{
-		return new String[] {"chennai","pune"};
-	}
+public class Welcomeservice {
+	
+
+		public String[] getcity()
+		{
+			return new String[] {"chennai","pune"};
+		}
 }
